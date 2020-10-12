79bbdddd911b319da2fe4e853fab9160519863ba
Hardik Raval <hardikraval1324@outlook.com>
2020-10-12 18:40:52 +0530

    Create turtel_exp.py

---
+++ turtel_exp.py
@@ -0,0 +1,16 @@
+import turtle
+
+lo=turtle.Turtle()
+
+lo.speed(0)
+lo.forward(100)
+lo.color("green")
+for i in range(100):
+    lo.right(165)
+    lo.forward(200)
+    lo.right(70)
+    lo.forward(100)
+
+
+
+turtle.done()
